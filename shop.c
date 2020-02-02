36f80551d1b931e230b378a3db0966e943886a17
DournauxNathan <dournauxnathan@gmail.com>
2020-02-02 15:12:55 +0100

    Struc Objet
    Ajout d'un structure contenant le nom et le prix des articles

--- shop.c
+++ shop.c
@@ -2,7 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 
+/*Structure des articles du magasin referencant leur nom et leurs prix*/
+struct Objet{
+	char nomObjet[30];
+	int prixObjet;
+};
+typedef struct Objet objet;
+
 int main()
 {
-	
+
 }
